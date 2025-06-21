8f76420350265c6c25b656f7128620d743e8f407
AsrafulMasum <h.m.asrafulmasum@gmail.com>
2025-06-21 10:07:47 +0600

    hosted server added to the base url

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://10.0.70.30:3002/api/v1",
+    baseUrl: "http://195.35.9.21:3001/api/v1",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("token");
       if (token && token !== "undefined") {
@@ -15,4 +15,4 @@ export const baseApi = createApi({
   endpoints: () => ({}),
 });
 
-export const imageUrl = "http://10.0.70.30:3002";
+export const imageUrl = "http://195.35.9.21:3001";
